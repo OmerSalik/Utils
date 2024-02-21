77acaf29b7001091f9f19e64a4e309a89fe0ae88
OmerSalik <95069290+OmerSalik@users.noreply.github.com>
2024-02-21 11:27:48 +0300

    Update utils.ts

--- utils.ts
+++ utils.ts
@@ -1,132 +1,148 @@
-export class Functions {
-  constructor() { }
-  isTimerActive: any = false;
-  timerName: any = null;
-  mf: any = (number: any) => Math.floor(number);
-  log: any = (...argList: any) => console.log(...argList);
-  table: any = (...content: any) => console.table(...content);
-  clear: any = () => console.clear();
-  qs: any = (selector: any) => document.querySelector(selector);
-  qsa: any = (selector: any) => document.querySelectorAll(selector);
-  dq: any = (selector: any) => document.querySelector(selector);
-  dqa: any = (selector: any) => document.querySelectorAll(selector);
-  dqs: any = (selector: any) => document.querySelector(selector);
-  dqsa: any = (selector: any) => document.querySelectorAll(selector);
-  bekle: any = (sure: any) => new Promise(resolve => setTimeout(()=>resolve(true), sure * 1000));
-  w: any = (sure: any) => new Promise(resolve => setTimeout(()=>resolve(true), sure * 1000));
-  gid: any = (id: any) => document.getElementById(id);
-  hidden: any = (element: any) => element.hidden=!element.hidden;
-  display: any = (element: any, d:any = 'block') => element.style.display=element.style.display==d?'none':d;
-  rs: any = (min: any, max: any) => this.mf(Math.random() * (( max + 1 ) - min)) + min;
-  lerp: any = (a: any, b: any, c: any) => a + (b - a) * c;
-  ga: any = (el: any, attribute: any) => el.getAttribute(attribute);
-  sa: any = (el: any, attribute: any, newValue: any) => el.setAttribute(attribute, newValue);
-  get: any = (itemName: any) =>localStorage.getItem(itemName);
-  set: any = (itemName: any, value: any) =>localStorage.setItem(itemName, value);
-  del: any = (itemName: any) => localStorage.removeItem(itemName);
-  limitText: any = (text: any, number: any) => {
-    let ttr = '';
-    for (let i = 0; i < number; i++) ttr += text[i];
-    return ttr;
-  }
-  mailOrPhone: any = (text: any) => {
-    text = text.replace(/\s/g, '');
-    let isThisMail = /[^0-9+()]/g.test(text), data;
-    if(isThisMail) {
-      text = text.toLowerCase();
-      if(!text.includes('@')) return { Error: "Mail must contain '@'"}
-      else if(text.length < 7 || text.split('@')[0].length < 1 || text.split('@')[1].length < 5) return { Error: "Please enter a valid E-Mail address."}
-      else data = text;
-    } else {
-      text = text.replace('(','').replace(')','');
-      if(text[0] == '5' && text.length == 10) data = '+90 (' + text.slice(0, 3) + ') ' + text.slice(3, 6) + ' ' + text.slice(6, 10);
-      else if(text[0] == '0' && text.length == 11) data = '+90 (' + text.slice(1, 4) + ') ' + text.slice(4, 7) + ' ' + text.slice(7, 11);
-      else if(text[0] == '9' && text.length == 12) data = '+90 (' + text.slice(2, 5) + ') ' + text.slice(5, 8) + ' ' + text.slice(8, 12);
-      else if(text[0] == '+' && text.length == 13) data = '+90 (' + text.slice(3, 6) + ') ' + text.slice(6,9) + ' ' + text.slice(9, 13);
+import { NonNullableFormBuilder } from "@angular/forms";
+
+class Functions {
+    constructor() {
+      this.setKey();
+      this.whenLoad(this.setStyle);
     }
-    return {mailMi: isThisMail, DATA: data};
-  }
-  ilkHarfBuyut: any = (text: any) => {
-    let ttr = '';
-    if(typeof text == 'string') {
-      text.split(' ').forEach((word, index) => {
-        let Text = '';
-        let nword = word.slice(0, 1).toLocaleUpperCase() + word.slice(1);
-        ttr += `${index != 0 ? ' ' : ''}${nword}`;
+    Key: null|string = null;
+    Style: string = `#fixed-container, *[modal-name], #utilTimer { top: 0; left: 0; right: 0; bottom: 0; display: flex; position: fixed; align-items: center; justify-content: center; background-color: rgba(0, 0, 0, .3); } #utilTimer { color: green; font-weight: bold; font-size: xx-large; }`;
+    Timer: { active: boolean } = { active: false };
+    Variables: any = {};
+    c: any = () => console.clear();
+    clear: any = () => console.clear();
+    mf: any = (number: number) => Math.floor(number);
+    lerp: any = (a: number, b: number, c: number) => a + (b - a) * c;
+    gid: any = (id: string) => document.getElementById(id);
+    log: any = (...argList: any) => console.log(...argList);
+    redirect: any = (href: string) => window.location.href = href;
+    get: any = (itemName: string) =>localStorage.getItem(itemName);
+    table: any = (...content: any) => console.table(...content);
+    qs: any = (selector: string) => document.querySelector(selector);
+    ga: any = (el: any, attribute: string) => el.getAttribute(attribute);
+    hidden: any = (element: any) => element.hidden=!element.hidden;
+    del: any = (itemName: string) => localStorage.removeItem(itemName);
+    qsa: any = (selector: string) => document.querySelectorAll(selector);
+    set: any = (itemName: string, value: any) =>localStorage.setItem(itemName, value);
+    error: any = (...errorList: any) => console.error('\n', ...errorList, '\n');
+    rs: any = (min: number, max: number) => this.mf(Math.random() * (( max + 1 ) - min)) + min;
+    cac: any = (id: string = 'canvas') => [this.gid(id), this.gid(id).getContext('2d')];
+    sa: any = (el: any, attribute: string, newValue: any) => el.setAttribute(attribute, newValue);
+    bigFirstLetter: any = (text: string) => text[0].toUpperCase() + text.slice(1, text.length);
+    w: any = (sure: number) => new Promise(resolve => setTimeout(()=>resolve(true), sure * 1000));
+    bekle: any = (sure: number) => new Promise(resolve => setTimeout(()=>resolve(true), sure * 1000));
+    la: any = (array: Array<any>, fun: Function = function() {} ) => array.forEach(element => this.log(fun ? fun(element) : element));
+    display: any = (element:  any, d='block') => element.style.display=element.style.display==d?'none':d;
+    variables: any = (key: string, value: any) => {if(value) this.Variables[key]=value;return this.Variables[key];};
+    adv: any = (selector: string, event: string, fun: Function = function() {}) => this.qsa(selector).forEach((el: any) => el.addEventListener(event, fun));
+    setStyle: any = () => this.gid('utilStyle') ? this.gid('utilStyle').innerHTML = this.Style : document.body.innerHTML += `<style id="utilStyle">${this.Style}</style>`;
+    modal: any = (modalName: string) => this.qs(`[modal-name="${modalName}"]`) ? this.qs(`[modal-name="${modalName}"]`).style.display == 'none' ? 'flex' : 'none' : this.error(`Modal '${modalName}' not found.`);
+    warn: any = (...warnList: any) => console.warn(...warnList);
+    selector: any = (el: any) => {
+      let [ ttr, atb ] = [ el.tagName, el.attributes ]
+      ttr += el.id ? '#' + el.id : ''; ttr += el.classList ? '.' + el.classList.value.replace(' ', '.') : '';
+      for (let i = 0; i < atb.length; i++) !['class', 'id'].includes(atb[i].name) ? ttr += `[${atb[i].name}="${atb[i].value}"]` : false;
+      return ttr;
+    }
+    setKey: any = async () => {
+      let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+      let key = '';
+      for (let i = 0; i < this.rs(16, 32); i++) key += letters.splice(this.rs(0, letters.length - 1), 1);
+      await this.w(.2);
+      this.Key = key;
+    }
+    token: any = (len: number = 16) => {
+      if(this.Key != null) {
+        let ttr = '';
+        for (let i = 0; i < len; i++) ttr += this.Key[this.rs(0, this.Key.length - 1)];
+        return ttr;
+      }
+      return new Promise(async (resolve) => {
+        await this.w(.1);
+        resolve(await this.token(len))
       })
     }
-    return ttr;
-  }
-  createToken(): any {
-    if (window.crypto && window.crypto.getRandomValues) {
-      const buffer = new Uint8Array(32);
-      window.crypto.getRandomValues(buffer);
-      const randomToken = Array.from(buffer, byte => byte.toString(16).padStart(2, '0')).join('');
-      return randomToken;
-    } else {
-      console.error('Tarayıcı rastgele isim oluşturma işlemini desteklemiyor. Bu sebeple a4Goo!3ub8fY atanıyor');
-      return 'a4Goo3ub8fY';
+    timer: any = (duration: number = 3, extraFeatures: any = { function: () => {}, type: 'second', style: {} }, ...argList: any) => {
+      return new Promise(async (resolve) => {
+        let { active } = this.Timer;
+        if(!active) {
+          this.Timer.active = true;
+          if(!extraFeatures.type) extraFeatures.type = 'second'; if(!extraFeatures.style) extraFeatures.style = {};
+          this.gid('utilTimer') ? this.gid('utilTimer').remove() : document.body.innerHTML += '<div id="utilTimer"></div>';
+          let el = this.gid('utilTimer');
+          Object.keys(extraFeatures.style).forEach(key => el.style[key] = extraFeatures.style[key]);
+          while (duration > 0) {
+            el.innerHTML = extraFeatures.type == 'second' ? duration : duration.toFixed(2);
+            await this.w(extraFeatures.type == 'second' ? 1 : .01);
+            duration -= extraFeatures.type == 'second' ? 1 : .01;
+          }
+          el.remove();
+          this.Timer.active = false;
+          extraFeatures.function && typeof(extraFeatures.function) == 'function' ? extraFeatures.function(...argList) : false;
+          resolve('Timer finished');
+          return;
+        } else {
+          this.error('Timer is already active');
+          resolve('Timer is already active');
+          return;
+        }
+      })
     }
-  }
-  timer: any = (time: any = 3, process: any, extraFeatures: any, isItFirstTime: any = true) => {
-    if(typeof(time) != 'number') time = 3;
-    time = Math.floor(time);
-    let id = this.timerName, color = 'green', backgroundColor = 'rgba(0,0,0,.2)';
-
-    if(typeof(extraFeatures) == 'object') {
-      id = extraFeatures.id ? extraFeatures.id : id;
-      color = extraFeatures.color ? extraFeatures.color : color;
-      backgroundColor = extraFeatures.backgroundColor ? extraFeatures.backgroundColor : backgroundColor;
-    }
-    
-    if(this.isTimerActive === false) {
-      time += 1;
-      id = this.createToken();
-      this.isTimerActive = true;
-      this.timerName = id;
-    }
-
-    let el = this.gid(this.timerName);
-
-    if(el) {
-      if(time == 1) {
-        this.isTimerActive = false;
-        el.innerHTML = '';
-        el.remove();
-        if(process && typeof(process) == 'function') process();
+    mailOrPhone: any = (text: string) => {
+      text = text.replace(/\s/g, '');
+      let isThisMail = /[^0-9+()]/g.test(text), data;
+      if(isThisMail) {
+        text = text.toLowerCase();
+        if(!text.includes('@')) return { Error: "Mail must contain '@'"}
+        else if(text.length < 7 || text.split('@')[0].length < 1 || text.split('@')[1].length < 5) return { Error: "Please enter a valid E-Mail address."}
+        else data = text;
+      } else {
+        text = text.replace('(','').replace(')','');
+        if(text.length < 10) return { error: 'Phone number must be at least 10 characters' };
+        else if(text[0] == '5' && text.length == 10) data = '+90 (' + text.slice(0, 3) + ') ' + text.slice(3, 6) + ' ' + text.slice(6, 10);
+        else if(text[0] == '0' && text.length == 11) data = '+90 (' + text.slice(1, 4) + ') ' + text.slice(4, 7) + ' ' + text.slice(7, 11);
+        else if(text[0] == '9' && text.length == 12) data = '+90 (' + text.slice(2, 5) + ') ' + text.slice(5, 8) + ' ' + text.slice(8, 12);
+        else if(text[0] == '+' && text.length == 13) data = '+90 (' + text.slice(3, 6) + ') ' + text.slice(6,9) + ' ' + text.slice(9, 13);
+        else return { error: 'Please enter a valid phone number.' };
       }
-      else {
-        time--;
-        el.innerHTML = `<div style="display:flex;justify-content:center;align-items:center;position:fixed;top:0;bottom:0;left:0;right:0;background-color:${backgroundColor};z-index:999;font-size:4em;font-weight: bold;color: ${color};">${time}</div>`;
-        setTimeout(()=>{this.timer(time, process, { id, color, backgroundColor }, false)},1000)
+      return { Type: data ? isThisMail ? 'mail' : 'tel' : 'null', DATA: data };
+    };
+    image: any = (source: string) => {
+      let image = new Image();
+      image.src = source;
+      return image;
+    };
+    getIntersection: any = (A: { x: number, y: number }, B: { x: number, y: number }, C: { x: number, y: number }, D: { x: number, y: number }) => {
+      let tTop: any = (D.x - C.x) * (A.y - C.y) - (D.y - C.y) * (A.x - C.x);
+      let uTop: any = (C.y - A.y) * (A.x - B.x) - (C.x - A.x) * (A.y - B.y);
+      let bottom: any = (D.y - C.y) * (B.x - A.x) - (D.x - C.x) * (B.y - A.y);
+      if (bottom != 0) {
+        let t = tTop / bottom;
+        let u = uTop / bottom;
+        if (t >= 0 && t <= 1 && u >= 0 && u <= 1) return {
+          x: this.lerp(A.x, B.x, t),
+          y: this.lerp(A.y, B.y, t),
+          offsetT: t,
+          offsetU: u
+        }
       }
-    } else {
-      let newElement = document.createElement('div');
-      newElement.setAttribute('id', this.timerName);
-      document.body.appendChild(newElement);
-      this.timer(time, process, extraFeatures, false);
-      return;
-    }
-  }
-  image: any = (source: any) => {
-    var image = new Image();
-    image.src = source;
-    return image;
-  }
-  getIntersection: any = (A: any, B: any, C: any, D: any) => {
-    const tTop = (D.x - C.x) * (A.y - C.y) - (D.y - C.y) * (A.x - C.x);
-    const uTop = (C.y - A.y) * (A.x - B.x) - (C.x - A.x) * (A.y - B.y);
-    const bottom = (D.y - C.y) * (B.x - A.x) - (D.x - C.x) * (B.y - A.y);
-    if (bottom != 0) {
-      const t = tTop / bottom;
-      const u = uTop / bottom;
-      if (t >= 0 && t <= 1 && u >= 0 && u <= 1) return {
-        x: this.lerp(A.x, B.x, t),
-        y: this.lerp(A.y, B.y, t),
-        offsetT: t,
-        offsetU: u
-      }
-    }
-    return null;
-  }
-}
+      return null;
+    };
+    whenLoad: any = async (nextProcess = function() {}, log = false) => {
+      return new Promise(async (resolve) => {
+        while(true) {
+          if(document.readyState === 'complete') {
+            resolve(true);
+            nextProcess();
+            if(log) console.log('load completed');
+            break;
+          } else {
+            await new Promise(res => setTimeout(res, 50))
+            if(log) console.log('still waiting for load...');
+          }
+        }
+      })
+    };
+  };
+  
+  const f = new Functions();
